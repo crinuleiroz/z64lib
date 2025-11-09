306b3a3bfa50d3be86054ca0b36195886c730cc8
crinuleiroz <usanimere@outlook.com>
2025-11-08 23:05:08 -0800

    Update z64lib.audioseq __init__.py

--- z64lib/audioseq/__init__.py
+++ z64lib/audioseq/__init__.py
@@ -2,3 +2,15 @@
 z64lib.audio.audioseq
 =====
 """
+
+from . import args
+from . import messages
+from . import sequence
+from . import parser
+
+__all__ = [
+    "args",
+    "messages",
+    "sequence",
+    "parser",
+]
