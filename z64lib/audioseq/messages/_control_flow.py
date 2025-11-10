9eed8baba5ee19f395ec8a6723c8537ada3219f6
crinuleiroz <usanimere@outlook.com>
2025-11-09 20:48:44 -0800

    Update _control_flow.py

--- z64lib/audioseq/messages/_control_flow.py
+++ z64lib/audioseq/messages/_control_flow.py
@@ -1,11 +1,8 @@
-from z64lib.audioseq.messages import (
-    AseqMessage, NoArgsMessage, ArgU8Message, ArgS8Message,
-    ArgU16Message, ArgS16Message, ArgVarMessage, PortamentoMessage
-)
+from z64lib.audioseq.messages import AseqMessage, NoArgsMessage, ArgU8Message, ArgU16Message, ArgVarMessage
 from z64lib.core.enums import AseqSection
 
 
-class AseqFlow_End(AseqMessage, NoArgsMessage):
+class AseqFlow_End(NoArgsMessage):
     """"""
     opcode = 0xFF
     is_terminal = True
@@ -14,12 +11,12 @@ class AseqFlow_End(AseqMessage, NoArgsMessage):
         return f'AseqFlow_End(opcode=0xFF)'
 
 
-class AseqFlow_Delay1(AseqMessage, NoArgsMessage):
+class AseqFlow_Delay1(NoArgsMessage):
     """"""
     opcode = 0xFE
 
 
-class AseqFlow_Delay(AseqMessage, ArgVarMessage):
+class AseqFlow_Delay(ArgVarMessage):
     """"""
     opcode = 0xFD
     sections = (AseqSection.META, AseqSection.CHAN)
@@ -28,69 +25,69 @@ class AseqFlow_Delay(AseqMessage, ArgVarMessage):
         return f'AseqFlow_Delay(delay={hex(self.args[0].value)})'
 
 
-class AseqFlow_Call(AseqMessage, ArgU16Message):
+class AseqFlow_Call(ArgU16Message):
     """"""
     opcode = 0xFC
     is_pointer = True
 
 
-class AseqFlow_Jump(AseqMessage, ArgU16Message):
+class AseqFlow_Jump(ArgU16Message):
     """"""
     opcode = 0xFB
     is_branch = True
 
 
-class AseqFlow_BranchEqual(AseqMessage, ArgU16Message):
+class AseqFlow_BranchEqual(ArgU16Message):
     """"""
     opcode = 0xFA
     is_branch = True
     is_conditional = True
 
 
-class AseqFlow_BranchLessThan(AseqMessage, ArgU16Message):
+class AseqFlow_BranchLessThan(ArgU16Message):
     """"""
     opcode = 0xF9
     is_branch = True
     is_conditional = True
 
 
-class AseqFlow_Loop(AseqMessage, ArgU8Message):
+class AseqFlow_Loop(ArgU8Message):
     """"""
     opcode = 0xF8
 
 
-class AseqFlow_LoopEnd(AseqMessage, NoArgsMessage):
+class AseqFlow_LoopEnd(NoArgsMessage):
     """"""
     opcode = 0xF7
 
 
-class AseqFlow_Break(AseqMessage, NoArgsMessage):
+class AseqFlow_Break(NoArgsMessage):
     """"""
     opcode = 0xF6
 
 
-class AseqFlow_BranchGreaterEqual(AseqMessage, ArgU16Message):
+class AseqFlow_BranchGreaterEqual(ArgU16Message):
     """"""
     opcode = 0xF5
     is_branch = True
     is_conditional = True
 
 
-class AseqFlow_JumpRelative(AseqMessage, ArgU8Message):
+class AseqFlow_JumpRelative(ArgU8Message):
     """"""
     opcode = 0xF4
     is_branch = True
     is_conditional = True
 
 
-class AseqFlow_JumpRelativeEqual(AseqMessage, ArgU8Message):
+class AseqFlow_JumpRelativeEqual(ArgU8Message):
     """"""
     opcode = 0xF3
     is_branch = True
     is_conditional = True
 
 
-class AseqFlow_JumpRelativeLessThan(AseqMessage, ArgU8Message):
+class AseqFlow_JumpRelativeLessThan(ArgU8Message):
     """"""
     opcode = 0xF2
     is_branch = True
