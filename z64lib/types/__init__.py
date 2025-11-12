eda3e3de5b2169ecd321da3389f1f3c0a654f7df
crinuleiroz <usanimere@outlook.com>
2025-11-12 06:25:11 -0800

    Update __init__.py

--- z64lib/types/__init__.py
+++ z64lib/types/__init__.py
@@ -6,7 +6,7 @@ A ctypes-like system for defining and parsing Zelda64 binary structs.
 """
 from .base import DataType
 from .primitives import (
-    u8, s8, u16, s16, u32, s32, f32
+    u8, s8, u16, s16, u32, s32, u64, s64, f32
 )
 from .references import (
     pointer
@@ -26,6 +26,8 @@ __all__ = [
     "s16",
     "u32",
     "s32",
+    "u64",
+    "s64",
     "f32",
     # References
     "pointer",
